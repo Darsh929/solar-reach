2726db45bed79158b28483989a6fc03aa068991e
Darsh <Darshagg2@gmail.com>
2026-03-30 21:30:09 -0500

    Removed images from team member entries
    Removed imageUrl properties for team members.

--- lib/team.ts
+++ lib/team.ts
@@ -2,26 +2,21 @@ export const team = [
   {
     name: "Aryan Surana",
     bio: "Aryan has a keen interest in just about anything scientific and volunteers at a nursing home every weekend. At Solar Reach, he is responsible for managing the team while also maintaining the solar lantern‘s design and development with Vedant.",
-    imageUrl: "/team/aryan.jpg",
   },
   {
     name: "Ethan Base",
     bio: "Ethan is a passionate volunteer with a knack for programming. At Solar Reach, he plays a key role in fundraising through social media platforms and communication with our partners.",
-    imageUrl: "/team/ethan.jpg",
   },
   {
     name: "Manashyu Kumar",
     bio: "I helped develop this website and support Solar Reach through outreach and event planning. I am really interested in technology and engineering and enjoy using those skills to make an impact.",
-    imageUrl: "/team/manashyu.jpg",
   },
   {
     name: "Vedant Aggarwal",
     bio: "Vedant greatly enjoys using engineering to benefit the lives of others. At Solar Reach, he has exemplified this by helping develop the model from scratch and helping with operations to keep the organization running.",
-    imageUrl: "/team/vedant.jpg",
   },
   {
     name: "Orlando Sirais",
     bio: "I helped make this website! I love computer science and am super excited to be part of Solar Reach working on the technical side of things.",
-    imageUrl: "/team/orlando.jpg",
   },
 ];
