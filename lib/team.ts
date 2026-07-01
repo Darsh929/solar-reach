bccee5077dd3a5c7782a1dc7d9fec1a438d4af6c
Darsh <Darshagg2@gmail.com>
2026-06-30 19:42:19 -0500

    Update Darsh Aggarwal's profile with new details

--- lib/team.ts
+++ lib/team.ts
@@ -4,8 +4,8 @@ export const team = [
     bio: "I have an interest in computers and web design. At Solar Reach, I manage the website.",
   },
   {
-    name: "Darsh Aggarwal",
-    bio: "Enter text here lol",
+    name: "Darsh Aggarwal | President",
+    bio: "I believe that engineering and hardware prototyping should be used to solve critical environmental challenges, which is why I work as the President of Solar Reach dedicated to delivering accessible clean energy solutions to communities in need. In this role, I oversee our technical initiatives and manage overall project coordination. Additionally, I focus on ensuring our projects create tangible impact through communities.",
   },
   {
     name: "Ruhan Sathyanath",
