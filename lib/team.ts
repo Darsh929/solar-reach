e7c470bc721e2d65dbad0d05b9777994be52c0e8
little guy <sanjeyarjun@gmail.com>
2026-07-07 18:30:30 -0500

    Update team.ts

--- lib/team.ts
+++ lib/team.ts
@@ -1,9 +1,8 @@
 export const team = [
   {
     name: "Sanjey Saravanan | Website and Distribution Manager ",
-    bio: "As the Website and Distribution Manager at Solar Reach, I combine my passion for engineering and technology with meaningful service by managing our website and helping coordinate the distribution of solar-powered lights to communities across Africa. I enjoy designing and improving digital experiences while ensuring our mission reaches as many people as possible. Through my work at Solar Reach, I strive to use innovation and problem-solving to help provide reliable, sustainable lighting to those who need it most.
-",
-  },
+    bio: "As the Website and Distribution Manager at Solar Reach, I combine my passion for engineering and technology with meaningful service by managing our website and helping coordinate the distribution of solar-powered lights to communities across Africa. I enjoy designing and improving digital experiences while ensuring our mission reaches as many people as possible. Through my work at Solar Reach, I strive to use innovation and problem-solving to help provide reliable, sustainable lighting to those who need it most.",
+},
   {
     name: "Darsh Aggarwal | President",
     bio: "I believe that engineering and hardware prototyping should be used to solve critical environmental challenges, which is why I work as the President of Solar Reach dedicated to delivering accessible clean energy solutions to communities in need. In this role, I oversee our technical initiatives and manage overall project coordination. Additionally, I focus on ensuring our projects create tangible impact through communities.",
